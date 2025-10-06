12a4ac03fe67e08babd4506352dc752484f22597
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-10-06 19:25:46 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Substitua pelo IP da máquina onde a API está rodando
-const API_URL = 'http://192.168.200.140'; 
+const API_URL = 'http://localhost'; 
 const API_PORTA = '3334';
 
 const api = axios.create({
