9a74cd27fc13ac75f39d0f52106458805e6c18ab
João Rafael Sanches <jsanchesedantas@gmail.com>
2025-10-14 22:34:45 -0300

    Revert "a"
    This reverts commit f18ad9f79f85632e4659c636073727eb07fc49d6.

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // Substitua pelo IP da máquina onde a API está rodando
-// const API_URL = 'http://10.67.23.36';
-const API_URL = 'http://10.67.23.35';
+const API_URL = 'http://10.67.23.36'; 
 const API_PORTA = '3334';
 
 const api = axios.create({
